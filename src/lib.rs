118d40ff877ea446f2cda63dcbc9e4aa53059cac
Derek Johnson <dej3tc@virginia.edu>
2023-05-20 20:00:30 -0400

    Build Tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-use std::borrow::BorrowMut;
 use std::cell::RefCell;
 use std::ops::DerefMut;
 use std::rc::Rc;
+use std::collections::VecDeque;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct TreeNode {
     pub val: i32,
     pub left: Option<Rc<RefCell<TreeNode>>>,
@@ -24,32 +24,30 @@ impl TreeNode {
             panic!("Can't pass an empty vector");
         }
 
-        let mut root = TreeNode::new(vals[0].unwrap());
-        let mut queue = vec![(&mut root, 0)];
-
-        while !queue.is_empty() {
-            let (node, idx) = queue.remove(0);
-
+        let root = Rc::new(RefCell::new(TreeNode::new(vals[0].unwrap())));
+        let mut queue = VecDeque::with_capacity(vals.len());
+        queue.push_back((Rc::clone(&root), 0));
+    
+        while let Some((node, idx)) = queue.pop_front() {
             let left_idx = idx * 2 + 1;
             let right_idx = idx * 2 + 2;
-
+            
+    
             if left_idx < vals.len() && vals[left_idx].is_some() {
-                let left_node = Rc::new(RefCell::new(TreeNode::new(
-                    vals[left_idx].unwrap(),
-                )));
-                node.borrow_mut().left = Some(left_node.clone());
-                queue.push((left_node.as_ref().borrow_mut().deref_mut(), left_idx));
+                let left_node = Rc::new(RefCell::new(TreeNode::new(vals[left_idx].unwrap())));
+                // set the left value of node to left_node
+                node.as_ref().borrow_mut().deref_mut().left = Some(Rc::clone(&left_node));
+                queue.push_back((Rc::clone(&left_node), left_idx));
             }
-
+    
             if right_idx < vals.len() && vals[right_idx].is_some() {
-                let right_node = Rc::new(RefCell::new(TreeNode::new(
-                    vals[left_idx].unwrap(),
-                )));
-                node.borrow_mut().left = Some(right_node.clone());
-                queue.push((right_node.as_ref().borrow_mut().deref_mut(), left_idx));
-            }
-        }
-        root
+                let right_node = Rc::new(RefCell::new(TreeNode::new(vals[right_idx].unwrap())));
+                // set the right value of node to right_node
+                node.as_ref().borrow_mut().deref_mut().right = Some(Rc::clone(&right_node));
+                queue.push_back((Rc::clone(&right_node), right_idx));
+            }        }
+    
+        Rc::try_unwrap(root).unwrap().into_inner()
     }
 }
 
@@ -64,5 +62,28 @@ mod test {
         assert_eq!(root.val, 6);
         assert_eq!(root.left.unwrap().borrow().val, 2);
         assert_eq!(root.right.unwrap().borrow().val, 8);
+    }
+    #[test]
+    fn ex2() {
+        let root = vec![Some(6), None, Some(8)];
+        let root = TreeNode::new_from_vec(root);
+        assert_eq!(root.val, 6);
+        assert!(root.left.is_none());
+        assert_eq!(root.right.unwrap().borrow().val, 8);
+    }
+    #[test]
+    fn ex3() {
+        let root = vec![Some(1), Some(2), Some(3), Some(4), Some(5), Some(6), Some(7)];
+        let root = TreeNode::new_from_vec(root);
+
+        assert_eq!(root.val, 1);
+        
+        // Check all the other values 
+        assert_eq!(root.left.as_ref().unwrap().borrow().val, 2);
+        assert_eq!(root.right.as_ref().unwrap().borrow().val, 3);
+        assert_eq!(root.left.as_ref().unwrap().borrow().left.as_ref().unwrap().borrow().val, 4);
+        assert_eq!(root.left.as_ref().unwrap().borrow().right.as_ref().unwrap().borrow().val, 5);
+        assert_eq!(root.right.as_ref().unwrap().borrow().left.as_ref().unwrap().borrow().val, 6);
+        assert_eq!(root.right.as_ref().unwrap().borrow().right.as_ref().unwrap().borrow().val, 7);
     }
 }
